aacd7aa0f5e8bcac14a94796dd96e33c7a1a1ac3
gabegor <gabegor94@gmail.com>
2020-01-27 16:46:19 +0200

    donald trump response

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
+let list=["https://ancient-fjord-32267.herokuapp.com/message/?message=","https://boto20.herokuapp.com/message/?message="]
+
 function getResponse() {
     let question = document.getElementById('q').value
     // console.log(question)
-    fetch(`https://ancient-fjord-32267.herokuapp.com/?message=${question}`).then((response) => {
+    fetch(`${list[1]}${question}`).then((response) => {
         return response.json();
     })
         .then((myJson) => {
@@ -9,3 +11,6 @@ function getResponse() {
             document.getElementById('res').innerHTML =  myJson.message;
         });
 }
+
+
+ 
